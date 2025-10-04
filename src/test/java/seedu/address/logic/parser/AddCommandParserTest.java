8c8f3fceb26fdf052b6b2579945cfa416c1e9502
nathantew14 <nathantew14@gmail.com>
2025-10-04 17:16:04 +0800

    Fix all missing remarks and passed all tests
    This took way too f*cking long

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -219,7 +219,7 @@ public class AddCommandParserTest {
 
         // two invalid values, only first invalid value reported
         assertParseFailure(parser, INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + INVALID_ADDRESS_DESC
-                + VALID_REMARK_BOB, Name.MESSAGE_CONSTRAINTS);
+                + REMARK_DESC_BOB, Name.MESSAGE_CONSTRAINTS);
 
         // non-empty preamble
         assertParseFailure(parser, PREAMBLE_NON_EMPTY + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
