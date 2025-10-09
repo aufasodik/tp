deb2850a30b63045555c7632de9133a6d625a5e3
soakedumplings <bridgetwutn@gmail.com>
2025-10-09 14:19:28 +0800

    Implement logic to execute single edit or batch edit

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -10,6 +10,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -27,6 +28,10 @@ public class EditCommandParser implements Parser<EditCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the EditCommand
      * and returns an EditCommand object for execution.
+     * Supports both single index editing (e.g., "1") and batch editing (e.g., "1,2,3").
+     * 
+     * @param args the user input arguments
+     * @return EditCommand configured for single or batch editing
      * @throws ParseException if the user input does not conform the expected format
      */
     public EditCommand parse(String args) throws ParseException {
@@ -34,10 +39,11 @@ public class EditCommandParser implements Parser<EditCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_COMPANY, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
 
-        Index index;
+        List<Index> indices;
 
         try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            // Parse indices - supports both single and comma-separated multiple indices
+            indices = ParserUtil.parseIndices(argMultimap.getPreamble());
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
         }
@@ -64,7 +70,12 @@ public class EditCommandParser implements Parser<EditCommand> {
             throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
         }
 
-        return new EditCommand(index, editCompanyDescriptor);
+        // Use method overloading: single index or multiple indices
+        if (indices.size() == 1) {
+            return new EditCommand(indices.get(0), editCompanyDescriptor);
+        } else {
+            return new EditCommand(indices, editCompanyDescriptor);
+        }
     }
 
     /**
