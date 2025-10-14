d3e901e283877049cb2faf9d53ade13c43a84e60
kzfsg <131367782+kzfsg@users.noreply.github.com>
2025-10-15 00:15:00 +0800

    Add filter command for company status
    Introduces FilterCommand and FilterCommandParser to allow filtering companies by application status.
    
    Updates AddressBookParser to support the new 'filter' command.

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,56 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.company.Status;
+
+/**
+ * Filters and lists all companies that match a given application status.
+ */
+public class FilterCommand extends Command {
+
+    public static final String COMMAND_WORD = "filter";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters companies by application status and displays "
+            + "them as a list with index numbers.\n"
+            + "Parameters: s/STATUS (one of: to-apply, applied, oa, tech-interview, hr-interview, in-process, "
+            + "offered, accepted, rejected)\n"
+            + "Example: " + COMMAND_WORD + " s/in-process";
+
+    private final Status status;
+
+    public FilterCommand(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredCompanyList(company -> company.getStatus().equals(status));
+        return new CommandResult(String.format(
+                Messages.MESSAGE_COMPANIES_LISTED_OVERVIEW, model.getFilteredCompanyList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof FilterCommand)) {
+            return false;
+        }
+        FilterCommand otherCommand = (FilterCommand) other;
+        return status.equals(otherCommand.status);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("status", status)
+                .toString();
+    }
+}
+
